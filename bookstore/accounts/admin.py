07a3b54f19300e32036b490870cdab84b9be3a5b
deepanshuSoni22 <deepanshusoni022@gmail.com>
2025-04-20 00:58:24 +0530

    Regsiter models in admin panel

--- bookstore/accounts/admin.py
+++ bookstore/accounts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_active', 'is_admin')
+    list_filter = ('is_admin', 'is_staff', 'is_active')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('email',)}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_admin', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+    search_fields = ('username', 'email')
+    ordering = ('username',)
